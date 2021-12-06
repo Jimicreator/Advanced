93d14e277e1c0c9c58b3736c6fbc45697fb3b0db
Jimicreator <94049086+Jimicreator@users.noreply.github.com>
2021-12-06 08:47:12 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -155,7 +155,7 @@ async def auto_filter(bot: Client, update: Message):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"😎 Page 1/{len_result if len_result < max_pages else max_pages} 😎", callback_data="ignore")
         ])
         
         
@@ -184,13 +184,13 @@ async def auto_filter(bot: Client, update: Message):
                 if ((len(ibuttons)%2) == 0):
                     ibuttons.append(
                         [
-                            InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=invite_link)
+                            InlineKeyboardButton(f"❤️ {chat_name} ❤️", url=invite_link)
                         ]
                     )
 
                 else:
                     ibuttons[-1].append(
-                        InlineKeyboardButton(f"⚜ {chat_name} ⚜", url=invite_link)
+                        InlineKeyboardButton(f"❤️ {chat_name} ❤️", url=invite_link)
                     )
                 
             for x in ibuttons:
@@ -206,7 +206,7 @@ async def auto_filter(bot: Client, update: Message):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Found {(len_results)} Results For Your Query: <code>{query}</code>",
+                text=f"Found {(len_results)} Showing Results For Your Query 🙂: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
